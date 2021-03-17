985099cc6e67c1b9b995625ffcb589ba32060e21
Appdest <bkokkula@appdest.com>
2021-03-17 16:56:19 +0530

    connectivity changes

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.testproject
 
 import android.content.Context
 import android.net.ConnectivityManager
-import androidx.appcompat.app.AppCompatActivity
+import android.net.NetworkCapabilities
+import android.net.NetworkInfo
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.testproject.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainViewModel: MainActivityViewModel
@@ -20,13 +24,14 @@ class MainActivity : AppCompatActivity() {
         binding.rvMesages.layoutManager = LinearLayoutManager(this)
         mainViewModel.appdb(this)
 
-        if(!basecheckConnection()) {
+        if(isNetworkConnected()) {
             mainViewModel.getmessages()?.observe(this, Observer {
-                if(it!=null )
+                if (it != null)
                     mainViewModel.savedata(it)
                 binding.rvMesages.adapter = it.messages?.let { it1 -> MainAdapter(it1) }
                 binding.tvDoctor.text = it.consultation_request?.doctor_name
-                binding.tvStatus.text = if(it.consultation_request?.consent?:false) "Online" else "Offline"
+                binding.tvStatus.text = if (it.consultation_request?.consent
+                                ?: false) "Online" else "Offline"
             })
         } else {
             mainViewModel.getofflinemessages()?.observe(this, Observer {
@@ -38,8 +43,18 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-    fun basecheckConnection():Boolean {
+    private fun isNetworkConnected(): Boolean {
         val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-       return cm.isActiveNetworkMetered
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            val activeNetwork = cm.activeNetwork
+            val nc = cm.getNetworkCapabilities(activeNetwork)
+            nc != null && (nc.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || nc.hasTransport(
+                    NetworkCapabilities.TRANSPORT_WIFI
+            ))
+        } else {
+            val activeNetwork = cm.activeNetworkInfo
+            activeNetwork != null && activeNetwork.isConnectedOrConnecting
+        }
     }
 }
